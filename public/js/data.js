8d38a9bacaccc09cbb52284f31e3c617bb31e4ac
abaheti <Anurag.BAHETI@amadeus.com>
2021-05-28 18:29:30 +0530

    Added Mumbai Location and little updated UI

---
+++ public/js/data.js
@@ -0,0 +1,5 @@
+var locations = ['Thane, Maharashtra', 'Mumbai, Maharashtra'];
+var urls = [
+'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=392&date=',
+'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=395&date='
+];
